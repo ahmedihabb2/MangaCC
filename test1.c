ae4f31372565c141ba31cff43e98f59a0764fe84
yousif-ahmed <yousif.lasheen60@gmail.com>
2023-04-29 04:06:37 +0300

    feat : testing not all works

---
+++ test1.c
@@ -0,0 +1,21 @@
+int main() {
+
+  float n1;
+  float n2;
+  float n3;
+  if (n1 >= n2 and n1 >= n3){
+
+    }endif
+
+    if (n2 >= n1 and n2 >= n3){
+
+    }endif
+      
+    if (n3 >= n1 and n3 >= n2){
+
+    }endif
+
+    return 0;
+
+  
+}
---
+++ test2.c
@@ -0,0 +1,21 @@
+int main()
+{
+    
+    float P = 1;
+    float R = 1;
+    float T = 1;
+ 
+    float SI = (P * T * R) / 100 +1112 %5 ;
+ 
+ 
+    return 0;
+}
+
+void main(int argc , char argv)   
+{
+    int i;
+    for (i = 0; i < argc; i= i+1)
+    {
+          
+    }
+}
