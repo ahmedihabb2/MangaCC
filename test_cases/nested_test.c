e39274d520f2b7d17811c5ce3d698179f8074eb8
We'am Bassem <58189568+We2Am-BaSsem@users.noreply.github.com>
2023-04-30 00:26:00 +0300

    test nested statements

---
+++ test_cases/nested_test.c
@@ -0,0 +1,149 @@
+
+while (5>3) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } endif
+} 
+
+
+if (true) {
+
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+
+} endif
+
+
+if (true) {
+
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+
+} else {
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+} 
+
+
+for (int i = 0; i < 10; i = i+1) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } endif
+}
+
+
+for (int i = 0; i < 10; i = i+1) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } else {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+}
+
+
+while (5>3) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } endif
+
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+
+}
+
+
+for (int i = 0; i < 10; i = i+1) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } endif
+
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+
+}
+
+
+if (true) {
+    if (false) {
+        int k = 5;
+    } endif
+
+    if (true) {
+        int k = 5;
+    } endif
+
+    if (true) {
+        int k = 5;
+    } else {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    }
+    
+} endif
+
+
+for (int i = 0; i < 10; i = i+1) {
+    int k = 5;
+    if (true) {
+        int k = 5;
+    } endif
+
+    while (5>3) {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } 
+
+    if (true) {
+        int k = 5;
+    } else {
+        int k = 5;
+    }
+
+    // if (true) {
+    //     int k = 5;
+    // } else if (true) {
+    //     int k = 5;
+    // } else {
+    //     int k = 5;
+    // }
+
+    repeat {
+        int k = 5;
+        if (true) {
+            int k = 5;
+        } endif
+    } until (true);
+
+}
