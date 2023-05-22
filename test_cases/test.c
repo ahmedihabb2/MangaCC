b50ef511ad9c84231d970e37982a8aa225aeb1d4
yousif-ahmed <yousif.lasheen60@gmail.com>
2023-05-23 01:49:10 +0300

    test enum quads

--- test_cases/test.c
+++ test_cases/test.c
@@ -1,47 +1,11 @@
-int x;
-print(x);
-int y = 5;
-print(y);
+enum color {RED, GREEN, BLUE};
 
-x = y;
-print(x);
+int main() {
+    enum color c = RED;
+    enum color d = GREEN;
+    if (c != d) {
+        c = BLUE;
+    }endif
 
-{
-    float x = 8.3;
-    print(x);
-        {
-            print(x);
-            int x = 5;
-            print(x);
-        }
-    print(x);
+    return 0;
 }
-x = true;
-y = 6;
-print(x);
-print(y);
-
-
-void main()
-{
-    int x = 5;
-    print(x);
-    {
-        int x = 6.6;
-        print(x);
-    }
-    print(x);
-}
-print(x);
-print(y);
-
-int func(float x, float y)
-{
-    x = 5.6;
-    y = false;
-    print(x);
-    print(y);
-    return x + y;
-}
-print(x);
-print(y);
